b6d17d7413816950a34cd67560d7980575c6463c
Kim-Hyunhee <tta_bi@naver.com>
2024-09-04 18:44:26 +0900

    Build: 프리즈마 설치 및 필요 폴더 생성

--- auth-server/src/main.ts
+++ auth-server/src/main.ts
@@ -43,6 +43,6 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
 
-  await app.listen(8000);
+  await app.listen(8888);
 }
 bootstrap();
--- keumbang-server/src/app.module.ts
+++ keumbang-server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './modules/order/order.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
